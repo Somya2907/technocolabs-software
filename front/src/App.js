52ba7fe4ff152e7ffedd785ec48fc7494e1fd8d6
Somya2907 <somyajain4019@gmail.com>
2022-08-05 16:03:34 +0530

    frontend timeline

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Appx from './components/App/App';
 import Task from './components/Tasks/Task';
+import Timeline from './components/Timeline/Timeline';
 
 function App() {
   return (
     <div className="App">
-      <Appx/>
+      <Timeline/>
     </div>
   );
 }
